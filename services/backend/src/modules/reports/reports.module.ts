905802d447c8c352e2ef59b9a35e0f5a76b734f9
tony-wang1990 <wangyatao678@gmail.com>
2026-06-02 21:59:25 +0800

    refactor: split reports backup service

--- services/backend/src/modules/reports/reports.module.ts
+++ services/backend/src/modules/reports/reports.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ReportsController } from './reports.controller'
+import { ReportsBackupService } from './reports-backup.service'
 import { ReportsService } from './reports.service'
 
-@Module({ controllers: [ReportsController], providers: [ReportsService] })
+@Module({ controllers: [ReportsController], providers: [ReportsService, ReportsBackupService] })
 export class ReportsModule {}
